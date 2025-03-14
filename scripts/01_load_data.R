e642b3275b9dbcb6cf742922f45011d2a80e6d82
yw-luo <luoyuwen99@gmail.com>
2025-03-14 02:37:54 -0700

    02_clean_transform_data script done & draft Makefile (refs #7 #8 #12)

--- scripts/01_load_data.R
+++ scripts/01_load_data.R
@@ -9,8 +9,8 @@ Usage:
   01_load_data.R --url=<url> --output_path=<output_path>
 
 Options:
-  --url=<url>       URL of the dataset to download.
-  --output_path=<output_path> Path to save the downloaded dataset.
+  --url=<url>                   URL of the dataset to download.
+  --output_path=<output_path>   Path to save the downloaded dataset.
 " -> doc
 
 opt <- docopt(doc)
@@ -26,5 +26,5 @@ download.file(opt$url, destfile = opt$output_path, mode = "wb")
 message("Data successfully downloaded and saved to ", opt$output_path)
 
 
-# run in terminal， the root directory
+# run the script in terminal (the root directory)
 # Rscript scripts/01_load_data.R --url="https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-03-04/longbeach.csv" --output_path="data/raw/longbeach.csv"
